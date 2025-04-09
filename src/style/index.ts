6f1b55ecd9ce5e0ec07032db2bd3a5b53fe20643
lacerdaaa <edulacerdaaa@gmail.com>
2025-04-09 18:01:29 -0300

    feat(): adding styles

---
+++ src/style/index.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5'
+    },
+    keyboardAvoidingView: {
+        flex: 1,
+    },
+    scrollView: {
+        flexGrow: 1,
+    },
+    content: {
+        flex: 1,
+        padding: 20,
+    }, 
+    title: {
+        fontSize: 28,
+        fontWeight: `bold`,
+        textAlign: 'center',
+        marginBottom:30,
+        color: '#333',
+    },
+    inputContainer: {
+        
+    }
+
+})
---
+++ src/utils/range.ts
@@ -0,0 +1,8 @@
+export const imc_ranges = [
+  { key: "Abaixo do peso", value: "Menos de 18.5" },
+  { key: "Peso normal", value: "Entre 18.5 e 24.9" },
+  { key: "Sobrepeso", value: "Entre 25 e 29.9" },
+  { key: "Obesidade Grau I", value: "Entre 30 e 34.9" },
+  { key: "Obesidade Grau II", value: "Entre 35 e 39.9" },
+  { key: "Obesidade Grau III", value: "Acima de 40" },
+];
