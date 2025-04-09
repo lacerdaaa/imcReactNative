bf6c04834b4ef581c30347112dc52204bf5f4944
lacerdaaa <edulacerdaaa@gmail.com>
2025-04-09 17:56:11 -0300

    feat(): get imc result func

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -1,3 +1,19 @@
 export const imacCalculo = (weigt: number, height: number): number => {
   return weigt / (height * height);
 };
+
+export const getIMCClassification = (imc: number): { classification: string; color: string } => {
+    if (imc < 18.5) {
+      return { classification: 'Abaixo do peso', color: '#3498db' };
+    } else if (imc < 25) {
+      return { classification: 'Peso normal', color: '#2ecc71' };
+    } else if (imc < 30) {
+      return { classification: 'Sobrepeso', color: '#f39c12' };
+    } else if (imc < 35) {
+      return { classification: 'Obesidade Grau I', color: '#e67e22' }; 
+    } else if (imc < 40) {
+      return { classification: 'Obesidade Grau II', color: '#e74c3c' }; 
+    } else {
+      return { classification: 'Obesidade Grau III', color: '#c0392b' }; 
+    }
+  };
